3a44b71ef5f537dbefa7965e6df2d77a8f3deded
millooo94 <camillomusmeciunict@gmail.com>
2023-04-18 14:49:40 +0200

    add other methods

--- la-mia-pizzeria-static/Api/PizzasController.cs
+++ la-mia-pizzeria-static/Api/PizzasController.cs
@@ -1,6 +1,7 @@
 ﻿using la_mia_pizzeria_static.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
 
 namespace la_mia_pizzeria_static.Api
 {
@@ -23,6 +24,65 @@ namespace la_mia_pizzeria_static.Api
                 .ToList();
 
             return Ok(pizzas);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetPizza(int id)
+        {
+            var pizza = _context.Pizzas.FirstOrDefault(p => p.Id == id);
+
+            if (pizza is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(pizza);
+        }
+
+        [HttpPost]
+        public IActionResult CreatePizza(Pizza pizza)
+        {
+            _context.Pizzas.Add(pizza);
+            _context.SaveChanges();
+
+            return Ok(pizza);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult PutPizza(int id, [FromBody] Pizza pizza)
+        {
+            var savedPizza = _context.Pizzas.FirstOrDefault(p => p.Id == id);
+
+            if (savedPizza is null)
+            {
+                return NotFound();
+            }
+
+            savedPizza.Name = pizza.Name;
+            savedPizza.Description = pizza.Description;
+            savedPizza.Price = pizza.Price;
+            savedPizza.Img = pizza.Img;
+            savedPizza.CategoryId = pizza.CategoryId;
+
+            _context.SaveChanges();
+
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeletePost(int id)
+        {
+            var savedPizza = _context.Pizzas.FirstOrDefault(p => p.Id == id);
+
+            if (savedPizza is null)
+            {
+                return NotFound();
+            }
+
+            _context.Pizzas.Remove(savedPizza);
+            _context.SaveChanges();
+
+            return Ok();
         }
     }
 }
